4a527bd947a41c244605c5410307e1d9b5efb4b8
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-26 23:34:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from decouple import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message
-import random
+from commands import *
+from pwdgen import pwdgen as pg
 
 firstclient = Client(
     "SampleBot",
@@ -15,14 +16,20 @@ firstclient = Client(
     api_hash = os.environ["API_HASH"]
 )
 
-start_string = "Hi {},\nI am a sample bot"
 
-@firstclient.on_message(filters.command(["start"]))
+@firstclient.on_message(filters.command(start_command))
 async def start(bot, update):
-  await update.reply_text(
-  text=start_string.format(update.from_user.mention)
-  )
-  
+    await update.reply_text(
+    text=start_msg.format(update.from_user.mention)
+    )
+
+@firstclient.on_message(filters.command(help_command))
+async def help(bot, update):
+    await update.reply_text(
+    text=help_msg
+    )
+
+'''
 @firstclient.on_message((filters.text | filters.forwarded | filters.reply) & filters.private)
 async def reply(bot, message):
   chat_id = int(message.chat.id)
@@ -30,5 +37,17 @@ async def reply(bot, message):
   ran_no = random.random()
   reply_text = f"**Here's is your message:**\n<code>{text}</code>\n\n**Here's your random number:**\n__{ran_no}__\n\nThank You!"
   await bot.send_message(text=reply_text, chat_id=chat_id)
-  
+'''
+
+@firstclient.on_message(filters.command(gen_random_pwd_command))
+async def gen_random_pwd(bot, update):
+    password = pg()
+    await update.reply_text(
+    text=gen_random_pwd_msg.format(password)
+    )
+    
+@firstclient.on_message((filters.text | filters.forwarded | filters.reply) & filters.private)
+async def reply(bot, message):
+    await bot.send_message(text="Working on that..!")
+
 firstclient.run()
