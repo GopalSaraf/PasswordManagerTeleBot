01d50016ed628c8e2389b6128e752f4efb4ccf91
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-26 23:05:57 +0530

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,31 @@
+# commands
+
+start_command = "start"
+help_command = "help"
+gen_random_pwd_command = "randpwd"
+gen_spec_pwd_command = "specpwd"
+my_pwd_command = "mypwds"
+
+
+# messages
+
+start_msg = "Hii {}!\nI am **password manager bot!**\nPress /help for more info."
+
+help_msg = """Save your passwords safely here..
+
+Here you can generate strong passwords and save them for future use.
+
+**You can generate password by two ways in here:**
+1| Generating password by random characters
+2| Generating password by your specific characters
+
+**Commands:** 
+- /randpwd <code><info></code>: For generating password by random characters.
+- /specpwd <code><info></code> <code><char></code>: For generating password by your specific characters. (<code><char></code> means characters you want to use in your passwords.)
+- /mypwds: For getting saved passwords.
+
+👆here <code><info></code> means for what you are generating password. This will be helpful to you for future use.
+
+Thank you!
+"""
+
