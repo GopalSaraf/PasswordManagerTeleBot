74f2334dfa20c18752bdd5ba17efe1bb7f7d3770
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-26 23:39:42 +0530

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -7,7 +7,7 @@ symbols = "!@#%&"
 
 universal_set = list(small_alphabets) + list(cap_alphabets) + list(numbers) + list(symbols)
 
-def pwdgen(length=8)
+def pwdgen(length=8):
     password = ''
     for i in range(int(length)):
         x = random.choice(universal_set)
