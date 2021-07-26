6faafbf8d7ea446af8be79fcab8091c4144e0b7f
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-26 23:23:20 +0530

    Create pwdgen.py

---
+++ pwdgen.py
@@ -0,0 +1,16 @@
+import random
+
+small_alphabets = "abcdefghijklmnopqrstuvwxyz"
+cap_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+symbols = "!@#%&"
+
+universal_set = list(small_alphabets) + list(cap_alphabets) + list(numbers) + list(symbols)
+
+def pwdgen(length)
+    password = ''
+    for i in range(int(length)):
+        x = random.choice(universal_set)
+        password += x
+        
+    return password
