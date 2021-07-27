bbd8b013c3b73f27e78a007900a8d99b8a6fe114
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-27 22:29:30 +0530

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -1,4 +1,6 @@
 import random
+from main import chat_id
+import datetime
 
 small_alphabets = "abcdefghijklmnopqrstuvwxyz"
 cap_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -14,3 +16,15 @@ def pwdgen(length=8,set=universal_set):
         password += x
         
     return password
+
+saved_pwds = f"{chat_id}.txt"
+
+def savepwd(pwd,info):
+    datetime = datetime.datetime()
+    pwd_line = f"Password: {pwd}\nPassword info: {info}\nPassword saved at {datetime}\n\n"
+    
+    with open(saved_pwds, 'r+') as file:
+        content = file.read()
+        file.seek(0, 0)
+        file.write(pwd_line.rstrip('\r\n') + '\n' + content)
+        file.close()    
