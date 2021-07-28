97a0a52202de5a25ec093f9e549275c6dad96140
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-28 20:51:49 +0530

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -7,7 +7,7 @@ symbols = "!@#%&"
 
 universal_set = list(small_alphabets) + list(cap_alphabets) + list(numbers) + list(symbols)
 
-def pwdgen(length=8,set=universal_set):
+def pwdgen(length=8, set=universal_set):
     password = ''
     for i in range(int(length)):
         x = random.choice(set)
@@ -16,15 +16,10 @@ def pwdgen(length=8,set=universal_set):
     return password
 
 
-def savepwd(pwd,info):
+def savepwd(file, pwd, info):
     import datetime
     dt = datetime.datetime.now()
     pwd_line = f"Password: {pwd}\nPassword info: {info}\nPassword saved at {dt}\n\n"
     
-    f = open("SavedPasswords.txt","w+")
-    f.close()
-    with open("SavedPasswords.txt", 'r+') as file:
-        content = file.read()
-        file.seek(0, 0)
-        file.write(pwd_line.rstrip('\r\n') + '\n' + content)
-        file.close()    
+    with open(file, 'a') as f:
+        print(pwd_line, file=f)
