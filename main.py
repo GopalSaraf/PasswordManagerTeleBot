aae712e8985017e75bcaf15b4dbc5a4f936a9675
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-28 21:26:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ async def reply(bot, message):
     # mypwds command
     if msg_list[0] == '/' + my_pwds_command[0]:
         with open(f"{chat_id}.txt") as file:
+            print('Hii', file=file)
             await bot.send_document(chat_id=chat_id, document=file, file_name='Passwords!')
 '''
 # mypwds command
