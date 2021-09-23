43e747cd4478cc15a3c5c0ed4bdc09cabfd8470e
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-09-23 19:45:23 +0530

    Update pwdmanager.py

--- pwdmanager.py
+++ pwdmanager.py
@@ -1,6 +1,7 @@
 import random
 import os
 import psycopg2
+import re
 
 small_alphabets = "abcdefghijklmnopqrstuvwxyz"
 cap_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -9,27 +10,35 @@ symbols = "!@#%&"
 
 universal_set = list(small_alphabets) + list(cap_alphabets) + list(numbers) + list(symbols)
 
+
 def pwdgen(length=12, set=universal_set):
     password = ''
     for i in range(int(length)):
         x = random.choice(set)
         password += x
-        
+
     return password
 
 
-conn = psycopg2.connect(
-    host = os.environ["DATABASE_HOST"],
-    database = os.environ["DATABASE"],
-    user = os.environ["DATABASE_USER"],
-    password = os.environ["DATABASE_PASSWORD"]
-)
+database_url = os.environ.get('DATABASE_URL')
 
+user = re.search('//(.*):', database_url).group(1).split(':')[0].strip()
+pwd = re.search(':(.*)@', database_url).group(1).split(':')[1].strip()
+host = re.search('@(.*):', database_url).group(1).strip()
+database = re.search(r'\d\d\d\d/(.*)', database_url).group(1).strip()
+
+conn = psycopg2.connect(
+    host=host,
+    database=database,
+    user=user,
+    password=pwd
+)
 
 
 def insert(user_id, pwd, info, username, saved_at):
     cur = conn.cursor()
-    cur.execute(f"insert into saved_passwords (chat_id, password, info, username, saved_at) values ({user_id}, '{pwd}', '{info}', '{username}', '{saved_at}')")
+    cur.execute(
+        f"insert into saved_passwords (chat_id, password, info, username, saved_at) values ({user_id}, '{pwd}', '{info}', '{username}', '{saved_at}')")
     conn.commit()
     cur.close()
 
@@ -39,7 +48,7 @@ def read_all(user_id):
     cur.execute(f"select * from saved_passwords where chat_id={user_id}")
     rows = cur.fetchall()
     cur.close()
-    
+
     return rows
 
 
@@ -52,5 +61,5 @@ def search(user_id, to_srch):
         if to_srch.lower() in row[2].lower():
             srch_rows.append(row)
     cur.close()
-    
+
     return srch_rows
